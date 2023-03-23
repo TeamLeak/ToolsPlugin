1c889dc9e5847ccf8819e2136145eb65aad18cc1
Leanfe <l.kapitman@gmail.com>
2023-03-23 12:37:57 +0200

    Small fix

--- src/main/java/com/github/leanfe/tools/ListenerManager.java
+++ src/main/java/com/github/leanfe/tools/ListenerManager.java
@@ -1,23 +1,22 @@
 package com.github.leanfe.tools;
 
 import com.github.leanfe.Application;
+import com.github.leanfe.config.Configuration;
 import com.github.leanfe.cryper.CryperListener;
 import com.github.leanfe.jump.JumpListener;
-
-import static com.github.leanfe.config.Constants.CreeperEnabled;
-import static com.github.leanfe.config.Constants.JumpEnabled;
 
 public class ListenerManager {
 
     private final Application instance = Application.getInstance();
 
     public void init() {
-        if (CreeperEnabled) {
+        if (Configuration.creeperModule.isCreeperEnabled()) {
             instance.getServer().getPluginManager().registerEvents(new CryperListener(), instance);
         }
 
-        if (JumpEnabled) {
+        if (Configuration.jumpModule.isJumpEnabled()) {
             instance.getServer().getPluginManager().registerEvents(new JumpListener(), instance);
         }
+
     }
 }
