341971b19249bf1e74ac3520e8a0f150d69fe2eb
Leanfe <l.kapitman@gmail.com>
2023-03-23 08:24:48 +0200

    I started working on code optimization, as well as work on neofetch for minecraft.

--- src/main/java/com/github/leanfe/Constants.java
+++ src/main/java/com/github/leanfe/Constants.java
@@ -4,26 +4,7 @@ import java.util.List;
 
 public class Constants {
 
-    public static boolean CreeperEnabled = Application.getInstance().getConfig().getBoolean("CreeperModule_enabled");
-    public static boolean JumpEnabled = Application.getInstance().getConfig().getBoolean("JumpModule_enabled");
-
-    public static boolean LeaveDamage = Application.getInstance().getConfig().getBoolean("LeaveDamage");
-
-    public static boolean DisableExplosions = Application.getInstance().getConfig().getBoolean("DisableExplosions");
-
-    public static List<String> CreeperBlocks = Application.getInstance().getConfig().getStringList("Creeper_blocks");
-
-    public static float JumpSpeed = Application.getInstance().getConfig().getInt("JumpSpeed");
-
-    public static float NormalSpeed = Application.getInstance().getConfig().getInt("NormalSpeed");
-
-    public static String JumpAccelerate = Application.getInstance().getConfig().getString("JumpAccelerate");
-
-    public static String JumpDecelerate = Application.getInstance().getConfig().getString("JumpDecelerate");
-
-    public static boolean OpEnabled = Application.getInstance().getConfig().getBoolean("OpModule_enabled");
-
-    public static String OpMessage = Application.getInstance().getConfig().getString("OpMessage");
+    // TODO: Дописать модуль крипера, так же перезагрузку конфига для опред.модуля!
 
     public static void reload() {
         CreeperEnabled = Application.getInstance().getConfig().getBoolean("CreeperModule_enabled");
@@ -48,4 +29,34 @@ public class Constants {
         OpMessage = Application.getInstance().getConfig().getString("OpMessage");
     }
 
+    private static class CreeperModule {
+        public static boolean CreeperEnabled = Application.getInstance().getConfig().getBoolean("CreeperModule_enabled");
+        public static boolean LeaveDamage = Application.getInstance().getConfig().getBoolean("LeaveDamage");
+        public static boolean DisableExplosions = Application.getInstance().getConfig().getBoolean("DisableExplosions");
+        public static List<String> CreeperBlocks = Application.getInstance().getConfig().getStringList("Creeper_blocks");
+
+    }
+
+    private static class JumpModule {
+        public static boolean JumpEnabled = Application.getInstance().getConfig().getBoolean("JumpModule_enabled");
+
+        public static float JumpSpeed = Application.getInstance().getConfig().getInt("JumpSpeed");
+
+        public static float NormalSpeed = Application.getInstance().getConfig().getInt("NormalSpeed");
+
+        public static String JumpAccelerate = Application.getInstance().getConfig().getString("JumpAccelerate");
+
+        public static String JumpDecelerate = Application.getInstance().getConfig().getString("JumpDecelerate");
+    }
+
+    private static class OpModule {
+        public static boolean OpEnabled = Application.getInstance().getConfig().getBoolean("OpModule_enabled");
+
+        public static String OpMessage = Application.getInstance().getConfig().getString("OpMessage");
+
+    }
+
+    private class FetchModule {
+
+    }
 }
